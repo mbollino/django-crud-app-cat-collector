b30b9b617459c6d9b03007416309e6ddd51f50f3
Michelle Bollino <jackandzoe.rosen@gmail.com>
2025-07-07 18:09:56 -0400

    Finish Part 2

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Cat
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Home page!</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def cat_index(request):
+    cats = Cat.objects.all()
+    return render(request, 'cats/index.html', {'cats': cats})
+
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
